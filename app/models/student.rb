a40b69cff87077abad6cbf33dd53c1810a6b082b
Mollie Dickenson <molliedickenson@gmail.com>
2021-06-20 11:49:37 -0400

    commit tree 9ca64125f3907552b23f0e8b0ab22548016b8301

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,8 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.search_by_name(name)
+    self.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}","%#{name}" )
+ end
+
 end
